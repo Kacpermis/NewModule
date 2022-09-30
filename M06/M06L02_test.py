1240b88fb39085b7614675b6abba9e3cdbe43e40
Kacpermis <kacpermis01@gmail.com>
2022-09-30 18:43:15 +0200

    Test for M06L02
    Test

--- M06/M06L02_test.py
+++ M06/M06L02_test.py
@@ -1,1 +1,5 @@
-from M06L02_list_comprehension_1 import 
+from M06L02_list_comprehension_1 import txt_search
+
+def test_txt_search():
+    got = txt_search(['a.zip', 'b.txt', 'txt'])
+    assert got == ['b.txt']
