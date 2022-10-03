bafe7d5f3c6e96bd9327aadabd04c77c3b0f4f78
Kacpermis <kacpermis01@gmail.com>
2022-10-03 05:46:14 +0200

    feat: class - def display

--- M06/M06L06_metody.py
+++ M06/M06L06_metody.py
@@ -27,3 +27,55 @@ print('Ziemniaki kosztowały', grosze, 'groszy.')
 ### 🔴 Ćwiczenie
 
 # W poprzednim ćwiczeniu zaimplementuj metodę RenameOperation.display(), która wyświetla operację, jaka zostanie wykonana. Wykorzystaj tę metodę w reszcie kodu.
+
+import os
+import glob
+
+
+pattern = input('Podaj pattern: ')
+ext = '.bak'
+files = glob.glob(pattern)
+operations = []
+
+class RenameOperation:
+    def __init__(self, old, new):
+        self.old = old
+        self.new = new
+
+    def display(self):
+        print(self.old, "-->", self.new)
+
+
+def ext_changing():
+    for x in files:
+        if '.' in x:
+            name = x.rsplit('.', 1)
+            nazwa, extension = name   
+        else:
+            name = x
+            extension = ""
+        new_filename = nazwa + ext
+        operation = RenameOperation(x, new_filename)
+        operations.append(operation)
+
+def show_change(operations):
+    print("Zostana dokonane nastepujace zmiany: ")
+    for op in operations:
+        RenameOperation.display()
+
+def execute_change(operations):
+    for op in operations:
+            os.rename(op.old, op.new)
+            print("Zmieniono", op.old, "--->", op.new)
+
+def main():
+    ext_changing()
+    show_change(operations)
+    zgoda = input('Kontynuowac? [t/n]')
+    if zgoda.lower() == 't':
+        execute_change(operations)
+    else:
+        print('Anulowano')
+
+if __name__ == "__main__":
+    main()
