b15ec4697fd69fdd0bf5c43c5f908171bc5f6a8e
Kacpermis <kacpermis01@gmail.com>
2022-10-03 05:48:45 +0200

    feat: add execute function in RenameOperation clas

--- M06/M06L07_metody_2.py
+++ M06/M06L07_metody_2.py
@@ -3,3 +3,59 @@
 ### 🔴 Ćwiczenie
 
 # W poprzednim ćwiczeniu dodaj metodę RenameOperation.execute(), która dokonuje faktycznej zmiany nazwy pliku, czyli wywołuje funkcję os.rename. Przepisz resztę kodu tak, aby wykorzystywała tę metodę.
+
+import os
+import glob
+
+
+pattern = input('Podaj pattern: ')
+ext = '.bak'
+files = glob.glob(pattern)
+operations = []
+
+class RenameOperation:
+    def __init__(self, old, new):
+        self.old = old
+        self.new = new
+
+    def display(self):
+        print(self.old, "-->", self.new)
+
+    def execute(self):
+        for op in operations:
+            os.rename(op.old, op.new)
+            print("Zmieniono", op.old, "--->", op.new)
+
+
+def ext_changing():
+    for x in files:
+        if '.' in x:
+            name = x.rsplit('.', 1)
+            nazwa, extension = name   
+        else:
+            name = x
+            extension = ""
+        new_filename = nazwa + ext
+        operation = RenameOperation(x, new_filename)
+        operations.append(operation)
+
+def show_change(operations):
+    print("Zostana dokonane nastepujace zmiany: ")
+    for op in operations:
+        RenameOperation.display()
+
+def execute_change(operations):
+    RenameOperation.execute()
+        
+
+def main():
+    ext_changing()
+    show_change(operations)
+    zgoda = input('Kontynuowac? [t/n]')
+    if zgoda.lower() == 't':
+        execute_change(operations)
+    else:
+        print('Anulowano')
+
+if __name__ == "__main__":
+    main()
