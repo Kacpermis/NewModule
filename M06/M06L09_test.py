c7bfc2ff81635c7f870562aa43831e7969de088e
Kacpermis <kacpermis01@gmail.com>
2022-10-05 05:31:28 +0200

    fix: collect_operation instead of ext_changing def

--- M06/M06L09_test.py
+++ M06/M06L09_test.py
@@ -1,21 +1,26 @@
-from M06L09_eq import ext_changing, RenameOperation
+from M06L09_eq import collect_operation, RenameOperation
 
-def test_ext_changing_normal_file():
-    got = ext_changing('file.txt')
+def test_collect_operation_normal_file():
+    got = collect_operation('file.txt')
     expected = RenameOperation('file.txt', 'file.bak')
     assert got == expected
 
-def test_ext_changing_missing_extension():
-    got = ext_changing('file')
+def test_collect_operation_missing_extension():
+    got = collect_operation('file')
     expected = RenameOperation('file', 'file.bak')
     assert got == expected
 
-def test_ext_changing_multiple_dots():
-    got = ext_changing('long.name.txt')
+def test_collect_operation_multiple_dots():
+    got = collect_operation('long.name.txt')
     expected = RenameOperation('long.name.txt', 'long.name.bak')
     assert got == expected
 
-def test_ext_changing_empty_extension():
-    got = ext_changing('file.')
+def test_collect_operation_empty_extension():
+    got = collect_operation('file.')
     expected = RenameOperation('file.', 'file.bak')
+    assert got == expected
+
+def test_collect_operation_paths():
+    got = collect_operation('directory.with.dots/file.txt')
+    expected = RenameOperation('directory.with.dots/file.txt', 'directory.with.dots/file.bak')
     assert got == expected
