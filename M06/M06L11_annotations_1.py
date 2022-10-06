fc83754797340cb944f142a818a5c45781f3bb19
Kacpermis <kacpermis01@gmail.com>
2022-10-06 05:13:46 +0200

    feat: annotation

--- M06/M06L11_annotations_1.py
+++ M06/M06L11_annotations_1.py
@@ -17,3 +17,69 @@ print(add('asdf', 'qwer'))  # Zwróć uwagę, że adnotacje typów to jedynie ad
 ### 🔴 Ćwiczenie
 
 # Dodaj adnotacje typów w M06L09.
+
+from gc import collect
+import os
+import glob
+EXTENSION = '.bak'
+
+class RenameOperation:
+    def __init__(self, old: str, new: str):
+        self.old = old
+        self.new = new
+
+    def execute(self):
+            os.rename(self.old, self.new)
+
+    def __str__(self):
+        return f"{self.old} -> {self.new}"
+    
+    def _repr__(self):
+        return f"RenameOperation(old={self.old!r}, new={self.new!r}"
+
+    def __eq__(self, other):
+        return self.old == other.old and self.new == other.new
+
+            
+
+
+def ext_changing(filename: str) -> list:
+    if '.' in filename:
+        tokens = filename.rsplit('.', 1)
+        name, extension = tokens   
+    else:
+        name = filename
+        extension = ""
+    return [name, extension]
+
+def collect_operation(filename: str) -> RenameOperation:
+        name, extension = ext_changing(filename)
+        new_filename = name + EXTENSION
+        operation = RenameOperation(filename, new_filename)
+        return operation
+
+def show_change(operations: list) -> None:
+    print("Zostana dokonane nastepujace zmiany: ")
+    for op in operations:
+        print(op)
+
+def execute_change(operations) -> None:
+    for op in operations:
+        op.execute()
+        print("Zmieniono", op.old, "--->", op.new)
+        
+
+def main() -> None:
+    pattern = input('Podaj pattern: ')
+    filenames = glob.glob(pattern)
+    operations = [collect_operation(filename) for filename in filenames]
+
+    show_change(operations)
+    zgoda = input('Kontynuowac? [t/n]')
+    if zgoda.lower() == 't':
+        execute_change(operations)
+    else:
+        print('Anulowano')
+
+if __name__ == "__main__":
+    main()
