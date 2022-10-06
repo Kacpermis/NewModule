d8f3777a2b03151a0f2df5d7fa8e52622027edd9
Kacpermis <kacpermis01@gmail.com>
2022-10-06 04:44:52 +0200

    feat: TodoItem class, ToDoItem function

--- M06/M06L10_csv.py
+++ M06/M06L10_csv.py
@@ -19,18 +19,27 @@ import csv
 
 # Każde zadanie składa się z id, opisu oraz informacji, czy zadanie zostało już wykonane (znak 'x'), czy jeszcze nie (znak '-').
 import csv
+TODOS_FILENAME = 'M06\\todos.csv'
 
+class TodoItem:
+    def __init__(self, id, description, done):
+        self.id = id
+        self.description = description
+        self.done = done
 
-class RenameOperation:
-    def __init__(self, toDo, Done):
-        self.old = toDo
-        self.new = Done
+def read_toDo(filename):
+    with open(filename) as stream:
+        read = csv.DictReader(stream)
+        todos = [ToDoItem_from_dict(row) for row in read]
+    return todos
 
+def ToDoItem_from_dict(row):
+    return TodoItem(
+        id=int(row['id']),
+        description=row(['desc']),
+        done=(row['done'] == 'x'),
+    )
 
-def read_toDo():
-    with open("M06\\todos.csv") as stream:
-        toDos = csv.DictReader(stream)
-        print(toDos)
 
 def description():
     print("  ID  DONE? DESCRIPTION")
