60da9db8dea6b63341babdf243f1f95d8cc7502d
Kacpermis <kacpermis01@gmail.com>
2022-10-06 05:30:36 +0200

    feat: annotation

--- M06/M06L13_annotations_3.py
+++ M06/M06L13_annotations_3.py
@@ -31,3 +31,51 @@ def process_expense(expense: Dict[str, Any]):
 ### 🔴 Ćwiczenie
 
 # Uzupełnij kod z poprzedniego ćwiczenia tak, aby uzupełnić brakujące adnotacje typów dotyczące samego słownika.
+import csv
+from typing import Dict, List
+TODOS_FILENAME = 'M06\\todos.csv'
+
+class TodoItem:
+    def __init__(self, id: int, description: str, done: bool) -> None:
+        self.id = id
+        self.description = description
+        self.done = done
+
+def read_toDo(filename: str) -> List[TodoItem]:
+    with open(filename) as stream:
+        read = csv.DictReader(stream)
+        todos = [ToDoItem_from_dict(row) for row in read]
+    return todos
+
+def ToDoItem_from_dict(row: Dict[str, str]) -> TodoItem:
+    return TodoItem(
+        id=int(row['id']),
+        description=row['desc'],
+        done=(row['done'] == 'x'),
+    )
+
+
+def description(todos: List[TodoItem]) -> None:
+    print("  ID  DONE? DESCRIPTION")
+    print('---- ----- ------------')
+    for todo in todos:
+        if todo.done:
+            done = 'x'
+        else:
+            done = '-'
+        print(f"{todo.id:6d} {done:^5} {todo.description}")
+    
+def summary(todos: List[TodoItem]) -> None:
+    done = len([t for t in todos if t.done])
+    all_ = len(todos)
+
+    print()
+    print(f"Number of fullfiled tasks: {done}/{all_}")
+
+def main() -> None:
+    todos = read_toDo(TODOS_FILENAME)
+    description(todos)
+    summary(todos)
+
+if __name__ == "__main__":
+    main()
