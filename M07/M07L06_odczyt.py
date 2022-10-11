be027814f40f77fccc3e423148c771e0cb485036
Kacpermis <kacpermis01@gmail.com>
2022-10-11 05:55:08 +0200

    feat: main function read

--- M07/M07L06_odczyt.py
+++ M07/M07L06_odczyt.py
@@ -2,15 +2,54 @@
 
 # Jak wczytać dane z pliku data.db z poprzedniej lekcji?
 
-import pickle
+# import pickle
 
-filename = 'data.db'
-with open(filename, 'rb') as stream:
-    obj_restored = pickle.load(stream)
+# filename = 'data.db'
+# with open(filename, 'rb') as stream:
+#     obj_restored = pickle.load(stream)
     
-print('obj_restored =', obj_restored)
+# print('obj_restored =', obj_restored)
 
 ### 🔴 Ćwiczenie
 
 # Napisz program, który będzie bazował na pliku todos.db stworzonym w poprzednim ćwiczeniu.
 # Załaduj plik do pamięci i wyświetl wszystkie zadania w formie tabeli.
+
+import pickle
+import sys
+
+import click
+
+DB_FILENAME = 'todos.db'
+
+class TodoItem:
+    def __init__(self, id, description, done):
+        self.id = id
+        self.description = description
+        self.done = done
+    
+    def __eq__(self,other):
+        return self.id == other.id and self.description == other.description and self.done == other.done
+    
+    def __repr__(self):
+        return f"TodoItem(id={self.id!r}, description={self.description!r}, done={self.done!r})"
+
+@click.command()
+def main():
+    try:
+        with open(DB_FILENAME, 'rb') as stream:
+            todos = pickle.load(stream)
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(1)
+    
+    print(f'=ID= DONE? ==DESC==')
+    for todo in todos:
+        if todo.done:
+            done = 'x'
+        else:
+            done = '-'
+        print(f'{todo.id:4} {done:^5} {todo.description}')
+
+if __name__ == "__main__":
+    main()
