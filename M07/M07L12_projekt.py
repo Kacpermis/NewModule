4720d7c1acac8c3000c402f2789df4808132ef66
Kacpermis <kacpermis01@gmail.com>
2022-10-13 05:37:05 +0200

    feat: new class and few funtions

--- M07/M07L12_projekt.py
+++ M07/M07L12_projekt.py
@@ -15,3 +15,53 @@
 # 6. Program przechowuje pomiędzy uruchomieniami bazę wszystkich wydatków w pliku budget.db. Zapisuj i wczytuj stan używając modułu pickle. Jeżeli plik nie istnieje, to automatycznie stwórz nową, pustą bazę. Zauważ, że nie potrzebujemy podpolecenia init.
 
 # 7. Wielkość wydatku musi być dodatnią liczbą. Gdzie umieścisz kod sprawdzający, czy jest to spełnione? W jaki sposób zgłosisz, że warunek nie jest spełniony?
+import csv
+from pickle import load, dump
+import sys
+
+from dataclasses import dataclass
+import click
+
+
+DB_FILENAME = "budget.db"
+BIG_EXPENSE = 1000
+
+@dataclass
+class Expenses:
+    id: int
+    amount: float
+    description: str
+
+    def __post_init__(self):
+        if self.amount <= 0:
+            raise ValueError('Amount cannot be zero or less')
+    
+    def is_big(self):
+        return self.amount >= BIG_EXPENSE
+
+
+def read_or_init():
+    try:
+        with open(DB_FILENAME, 'rb') as stream:
+            expenses = load(stream)
+    except FileNotFoundError:
+        expenses = []
+    return expenses
+
+def save_db(expenses):
+    with open(DB_FILENAME, 'wb') as stream:
+        expenses = dump(expenses, stream)
+
+def new_id(expenses):
+    ids = {expense.id for expense in expenses}
+    counter = 1
+    while counter in ids:
+        counter += 1 
+    return counter
+
+def total_expenses(expenses):
+    amounts = [e.amount for e in expenses]
+    return sum(amounts)
+
+
+    
